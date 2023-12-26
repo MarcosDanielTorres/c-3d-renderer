b8e1a8e0c3648cbaa98fdcd1163a9ede4679ce11
MarcosDanielTorres <marcoss.nqn@gmail.com>
2023-12-26 13:00:59 -0300

    fdsa:

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
+run:
+	gcc -Wall -std=c99 ./src/*.c -lSDL2 -o renderer
+	./renderer
+
 build:
 	gcc -Wall -std=c99 ./src/*.c -lSDL2 -o renderer
-
-run:
-	./renderer
 
 clean:
 	rm ./renderer
--- src/main.c
+++ src/main.c
@@ -1,69 +1,36 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
+
+#include "display.h"
+#include "vector.h"
+
+#define N_POINTS 9 * 9 * 9
 
 bool is_running = false;
 
-SDL_Window* window = NULL;
-uint32_t window_width = 800;
-uint32_t window_height = 600;
+vec3_t* vectors;
+vec2_t* projected_points;
 
-SDL_Renderer* renderer = NULL;
+uint32_t RED = 0xFFFF0000;
+uint32_t GREEN = 0xFF00FF00;
+uint32_t BLUE = 0xFF0000FF;
+uint32_t YELLOW = 0xFFFFFF00;
+uint32_t WHITE = 0xFFFFFFFF;
+uint32_t BLACK = 0xFF000000;
 
-SDL_Texture* color_buffer_texture = NULL;
-uint32_t* color_buffer = NULL;
+vec2_t project(vec3_t point) {
+   vec2_t projected_point = {
+       .x = point.x,
+       .y = point.y
+   };
 
-
-bool initialize_window(void) {
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        fprintf(stderr, "Error initializing SDL.\n");
-        return false;
-    }
-
-    /*
-     * SDL_DisplayMode display_mode;
-     * SDL_GetCurrentDisplayMode(0, &display_mode);
-     * window_width = display_mode.w;
-     * window_height = display_mode.h;
-     *
-     * window_width = 800;
-     * window_height = 600;
-     */
-
-
-    window = SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, window_width, window_height, 0);
-    if (!window) {
-        fprintf(stderr, "Error creating SDL window.\n");
-        return false;
-    }
-
-    renderer = SDL_CreateRenderer(window, -1, 0);
-    if (!renderer) {
-        fprintf(stderr, "Error creating SDL renderer.\n");
-        return false;
-    }
-
-    // SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
-
-    return true;
+   return projected_point;
 }
 
-void setup(void){
-    is_running = initialize_window();
 
-    color_buffer = malloc(window_width * window_height * sizeof(uint32_t));
-
-    color_buffer_texture = SDL_CreateTexture(
-        renderer,
-        SDL_PIXELFORMAT_ARGB8888,
-        SDL_TEXTUREACCESS_STREAMING,
-        window_width,
-        window_height
-    );
-}
-
-void process_input(void){
+void process_input(void) {
     SDL_Event event;
     SDL_PollEvent(&event);
 
@@ -72,67 +39,98 @@ void process_input(void){
             is_running = false;
             break;
         case SDL_KEYDOWN:
-            if (event.key.keysym.sym == SDLK_ESCAPE) {
+            if (event.key.keysym.sym == SDLK_ESCAPE)
                 is_running = false;
-            }
             break;
     }
 }
 
-void update(void){
-
-}
-
-void clear_color_buffer(uint32_t color) {
-    for (int i = 0; i < window_height; i++) {
-        for (int j = 0; j < window_width; j++) {
-            color_buffer[(window_width * i) + j] = color;
-        }
+void update(void) {
+    for (int i = 0; i < N_POINTS; i++) {
+        vec3_t point = vectors[i];
+        vec2_t projected_point = project(point);
+        projected_points[i] = projected_point;
     }
 }
 
-void render_color_buffer() {
-    SDL_UpdateTexture(
-        color_buffer_texture,
-        NULL,
-        color_buffer,
-        (int) (window_width * sizeof(uint32_t))
-    );
-    SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
-}
-
-void draw_grid(uint32_t* color_buffer) {
-    uint32_t grid_color = 0xFF0000FF;
-    for (int i = 0; i < window_height; i++) {
-        for (int j = 0; j < window_width; j++) {
-            if (i % 10 == 0 || j % 10 == 0) {
-                color_buffer[(window_width * i) + j] = grid_color;
-            }
-        }
-    }
-}
-
-void render(void){
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+/*
+ *
+void render(void* callback) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
-    clear_color_buffer(0xFFFFFF00);
-    draw_grid(color_buffer);
+    callback();
+    // -- calback --
+    draw_grid();
+    draw_rect(300, 200, 300, 150, 0xFFFF00FF);
+    // -- calback --
+
     render_color_buffer();
+    clear_color_buffer(0xFF000000);
 
     SDL_RenderPresent(renderer);
 }
+ *
+ *
+ *
+ *
+ */
 
-void destroy(void) {
-    free(color_buffer);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+void render(void) {
+
+    draw_grid();
+    // draw_rect(300, 200, 300, 150, 0xFFFF00FF);
+    for (int i = 0; i < N_POINTS; i++) {
+        vec2_t projected_point = projected_points[i];
+
+        draw_rect(projected_point.x * 200 + (window_width / 2), projected_point.y * 200 + (window_height / 2), 4, 4, RED);
+        // draw_pixel(projected_point.x * 200 + 400, projected_point.y * 200 + 400, YELLOW);
+    }
+
+    render_color_buffer();
+    clear_color_buffer(0xFF000000);
+    SDL_RenderPresent(renderer);
+}
+
+
+
+
+void init_vectors(float width, float height, float depth, int x_density, int y_density, int z_density) {
+    // width = 2
+    // height = 2
+    // depth = 2
+    float dx = width / 9.0; // 0.22222222
+    float dy = height / 9.0;
+    float dz = depth / 9.0;
+    float x_start = -1.0;
+    float y_start = -1.0;
+    float z_start = -1.0;
+
+    for(int z = 0; z < z_density; z++){ 
+        for(int y = 0; y < y_density; y++) {
+            for(int x = 0; x < x_density; x++) {
+                vec3_t new_point = { .x = x_start + dx * x, .y = y_start + dy * y, .z = z_start + dz * z };
+                printf("(%.3f, %.3f, %.3f)", new_point.x, new_point.y, new_point.z);
+                vectors[(x_density * y) + x + (z * x_density * y_density)] = new_point;
+            }
+        }
+    }
+    
 }
 
 int main(void) {
-    setup();
+    is_running = initialize_window();
+    int x_density = 9;
+    int y_density = 9;
+    int z_density = 9;
 
+    vectors = malloc(sizeof(vec3_t) * x_density * y_density * z_density);
+    projected_points = malloc(sizeof(vec2_t) * x_density * y_density * z_density);
+
+    init_vectors(2, 2, 2, 9, 9, 9);
+
+
+    setup();
 
     while (is_running) {
         process_input();
@@ -140,6 +138,9 @@ int main(void) {
         render();
     }
 
-    destroy();
+    free(vectors);
+    free(projected_points);
+    destroy_window();
+
     return 0;
 }
